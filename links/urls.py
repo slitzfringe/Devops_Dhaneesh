6a487b6a5a403d5f0f679e3bacfb270b7e91c7aa
slitzfringe <revanth.sivadas@gmail.com>
2024-07-06 22:49:17 +0530

    f3

--- links/urls.py
+++ links/urls.py
@@ -5,5 +5,5 @@ from .views import index, new, root_link
 urlpatterns = [
     path("", index, name="home"),
     path('<str:link_slug>/', root_link, name="root-link"),
-     path("link/create/", new, name="create_link"),
+    path("link/create/", new, name="create_link"),
 ]
